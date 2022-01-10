d1e6cc5ebbc74e408d4f692f164ea108fdad04c0
David Elias <36795720+davidelias346@users.noreply.github.com>
2022-01-10 18:49:10 -0300

    Add files via upload

---
+++ Dio_Codes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnoavationOne{
+
+    class Program{
+        static void Main(string[] args){
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i*2);
+            
+            
+            
+            }
+            
+        }
+    }
+}
